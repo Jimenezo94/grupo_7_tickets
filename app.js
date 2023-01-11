d4efc932d2a88445908a48307f8d7dc5f8a9e4e8
Juliana Jimenez <nani.jimenez@hotmail.com>
2023-01-10 21:59:10 -0500

    Add page index

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const app = express();
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 
