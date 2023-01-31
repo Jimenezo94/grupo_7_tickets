efbf3f90124ed4c698c893a86271aaa807821af0
Juliana Jimenez <nani.jimenez@hotmail.com>
2023-01-30 22:23:45 -0500

    templates-controllers

---
+++ src/routers/mainRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mainRouter = express.Router();
+const mainController = require('../controllers/mainController');
+
+mainRouter.get('/', mainController.index);
+
+
+module.exports = mainRouter;
---
+++ src/routers/userRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userRouter = express.Router();
+const userController = require('../controllers/userController');
+
+userRouter.get('/login', userController.login);
+userRouter.get('/register', userController.register);
+
+module.exports = userRouter;
